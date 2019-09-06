a603f2796a8f41a265e3f1315a5336cbd10c3995
dacsgb <dcolon@okstate.edu>
2019-09-06 11:19:00 -0500

    Started Gauss

--- Homework/HW 2/hw2d.py
+++ Homework/HW 2/hw2d.py
@@ -10,17 +10,22 @@ def GaussElim(Aaug):
         print("Dimensions are not valid")
         return None
 
+    for i in range(0,m):
+        for j in range(i+1,m):
+            f = (Aaug[j][i]/Aaug[i][i])
+            x[j] = x[j] - (f*x[i])
+
     return x
 def main():
 
     
 
     A = [[4,-1,1,3],[2,-3,1,9],[-1,1,7,-6]]
-    X = GaussSeidel(A)
+    X = GaussElim(A)
     print(X)
 
     C = [[4,3,1,-1,2],[2,-5,0,-2,-3],[-3,3,-6,1,5,],[0,1,4,8,-2]]
-    X = GaussSeidel(C)
+    X = GaussElim(C)
     print(X)
 
 
