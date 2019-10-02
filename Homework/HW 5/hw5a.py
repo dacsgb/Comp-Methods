f497d7cbcb95f08f81f9c7297116b279bbc05b4c
dacsgb <dcolon@okstate.edu>
2019-10-02 15:19:48 -0500

    Equation solves something

--- Homework/HW 5/hw5a.py
+++ Homework/HW 5/hw5a.py
@@ -1,13 +1,16 @@
 import numpy as np
+import scipy.optimize as opt
 
 def vel(Q):
     # Calculate velocities for every pipe
-    v = 4*Q/(np.pi*D**2)
+    v = np.zeros(len(Q)-1)
+    for i in range(len(Q)-1):
+        v[i] = 4*Q[i]/(np.pi*D[i]**2)
     return v
 
 def ReNum(v):
     # Calculate Reynolds number for each pipe
-    Re = (rho*abs(v)*D)/mu
+    Re= (rho*abs(v)*D)/mu
     return Re
 
 def del_p(f,L,v):
@@ -20,9 +23,27 @@ def fric(Re):
     f = (-2.0*np.log10( eps/(3.7065*D) - 5.0452/Re*np.log10( ((eps/D)**1.1098)/2.57 ) + 5.5506/(Re**0.8981)    ))**-2
     return f
 
-def sys(Q,Qin):
-    Qin[0]-Q[0]-Q[1]
-    Q[0]
+def err(Q):
+
+    E = np.zeros(1)
+    v = vel(Q)
+    Re = ReNum(v)
+    f = fric(Re)
+    p = del_p(f,L,v)
+
+    E[0] = Qin[0] -Q[0] -Q[1]
+    E = np.append(E,Q[0]-Q[2])
+    E = np.append(E,Q[1]-Q[3]-Q[5])
+    E = np.append(E,Q[3]-Q[4]-Q[6]-Qin[1])
+    E = np.append(E,Q[2]+Q[4]-Q[7])
+    E = np.append(E,Q[5]-Q[8]-Qin[2])
+    E = np.append(E,Q[6]+Q[8]-Q[9])
+    E = np.append(E,Q[7]+Q[9]-Q[10])
+    E = np.append(E,p[1]+p[3]+p[4]-p[2]-p[0])
+    E = np.append(E,p[5]+p[8]-p[6]-p[5])
+    E = np.append(E,p[6]+p[9]-p[7]-p[4])
+
+    return E
 
 rho = 1000 # density in kg/m^3
 mu =  0.00089 # viscosity in Ns/m^2
@@ -30,16 +51,9 @@ eps = 0.00025   # roughness of pipe in m
 D = np.array([300,200,200,200,200,150,150,150,250,250])*10**-3 # diameter of pipes in m
 L = np.array([300,100,100,125,125,100,100,100,125,125]) # length of pipes in m
 
-Q = np.array([1,1,1,1,1,1,1,1,1,1,1]) # flow in pipes and Qh in m/s
-Qin = np.array([60,30,15])*0.001 # flow entering and leaving in m/s
+Q = np.array([1,1,1,1,1,1,1,1,1,1,1]) # flow in pipes and Qh in m^3/s
+Qin = np.array([60,30,15])*0.001 # flow entering and leaving in m^3/s
 
+Qsol = opt.fsolve(err,[Q])
+print(Qsol*1000)
 
-
-
-v = vel(Q)
-Re = ReNum(v)
-f = fric(Re)
-dp = del_p(f,L,v)
-Qsys = sys(Q,Qin)
-
-print(dp)
